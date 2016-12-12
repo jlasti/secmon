9b9c59e22008d00f0b1a1d9d93bac3938212ee36
pes1899 <gulemitras@gmail.com>
2016-12-12 18:41:33 +0100

    [ADD] Dashboard itemy layout.

--- views/view/index.php
+++ views/view/index.php
@@ -18,14 +18,69 @@ $this->params['title'] = 'Dashboard';
     </div>
 
     <div class="grid">
-      <div class="blue lighten-4 grid-item">
-          <svg width="960" height="500"></svg>
+      <div class="grid-item card width2">
+        <div class="card-content">
+            <div class="card-header">
+                <span class="card-title activator grey-text text-darken-4">First with graph<i class="material-icons right">more_vert</i></span>
+            </div>
+            <div class="card-body">
+                <svg width="960" height="500"></svg>
+            </div>
+        </div>
+        <div class="card-reveal">
+            <div class="card-header">
+                <span class="card-title grey-text text-darken-4">First with graph - options<i class="material-icons right">close</i></span>
+            </div>
+            <div class="card-body">
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+        </div>
       </div>
-      <div class="blue lighten-4 grid-item width2">...</div>
-      <div class="blue lighten-4 grid-item">...</div>
+
+      <div class="grid-item card width2">
+        <div class="card-content">
+            <div class="card-header">
+                <span class="card-title activator grey-text text-darken-4">Second<i class="material-icons right">more_vert</i></span>
+            </div>
+            <div class="card-body">
+                <p>Obsah hocijaky <a href="#">This is a link</a></p>
+                <p>Obsah hocijaky <a href="#">This is a link</a></p>
+                <p>Obsah hocijaky <a href="#">This is a link</a></p>
+            </div>
+        </div>
+        <div class="card-reveal">
+            <div class="card-header">
+                <span class="card-title grey-text text-darken-4">Second - options<i class="material-icons right">close</i></span>
+            </div>
+            <div class="card-body">
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+        </div>
+      </div>
+
+      <div class="grid-item card">
+        <div class="card-content">
+            <div class="card-header">
+                <span class="card-title activator grey-text text-darken-4">Third<i class="material-icons right">more_vert</i></span>
+            </div>
+            <div class="card-body">
+                <p>Obsah hocijaky <a href="#">This is a link</a></p>
+                <p>Obsah hocijaky <a href="#">This is a link</a></p>
+                <p>Obsah hocijaky <a href="#">This is a link</a></p>
+            </div>
+        </div>
+        <div class="card-reveal">
+            <div class="card-header">
+                <span class="card-title grey-text text-darken-4">Third - options<i class="material-icons right">close</i></span>
+            </div>
+            <div class="card-body">
+                <p>Here is some more information about this product that is only revealed once clicked on.</p>
+            </div>
+        </div>
+      </div>
     </div>
     
-<?php Pjax::begin(); ?>    <?= GridView::widget([
+<!--<?php Pjax::begin(); ?>    <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
         'columns' => [
@@ -39,4 +94,6 @@ $this->params['title'] = 'Dashboard';
             ['class' => 'macgyer\yii2materializecss\widgets\grid\ActionColumn'],
         ],
     ]); ?>
-<?php Pjax::end(); ?></div>
+<?php Pjax::end(); ?>*-->
+
+</div>
