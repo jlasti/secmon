1364af66b706846862542b49c7790f0a9d7fdb92
simbiont123 <robofico123>
2019-04-15 18:29:06 +0200

    added config to anomaly script

---
+++ controllers/EventsNormalizedListController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\EventsNormalized;
+use Yii;
+use app\models\Event;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use app\models\EventsAnalyzedNormalizedListSearch;
+
+/**
+ * EventController implements the CRUD actions for Event model.
+ */
+class EventsNormalizedListController extends Controller
+{
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Lists all Event models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $searchModel = new EventsAnalyzedNormalizedListSearch();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Event model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModelNormalized($id),
+        ]);
+    }
+
+    /**
+     * Finds the Event model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Event the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Event::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+
+    /**
+     * @param $id
+     * @return EventsNormalized|null
+     * @throws NotFoundHttpException
+     */
+    protected function findModelNormalized($id)
+    {
+        if (($model = EventsNormalized::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
