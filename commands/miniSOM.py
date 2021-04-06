75f46570876048ecb03dc012090a52232efb7f7c
root <root@centos7.linuxvmimages.com>
2021-04-06 10:18:53 -0400

    Refactor miniSOM script

--- commands/miniSOM.py
+++ commands/miniSOM.py
@@ -1,46 +1,42 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# Place anomaly_daemon.service to /etc/systemd/system/ - run as every service
-# Change in anomaly_daemon.service working directory as you wish, defaul is /var/www/working_dir
-
 import datetime
 import pandas
-import numpy as np
 import psycopg2
 import re
+import numpy as np
 from collections import defaultdict
-from backports import configparser
 from sequence import pad_sequences
-from text import one_hot
+from backports import configparser
 from minisom import MiniSom
+from text import one_hot
 
 config = configparser.ConfigParser()
 config.read('/var/www/html/secmon/config/anomaly_config.ini')
 
-columns_to_analyze = config['DATA']['columns_to_analyze']
-clean_text = config.getboolean('MINISOM', 'clean_text')
-whole_text = config.getboolean('MINISOM', 'whole_text')
-whole_ip = config.getboolean('MINISOM', 'whole_ip')
-whole_mac = config.getboolean('MINISOM', 'whole_mac')
-som_shape = (int(config['MINISOM']['number_of_clusters_x']), int(config['MINISOM']['number_of_clusters_y']))
-number_of_iteration = int(config['MINISOM']['number_of_iteration'])
+columnsToAnalyze = config['DATA']['columns_to_analyze']
+cleanText = config.getboolean('MINISOM', 'clean_text')
+wholeText = config.getboolean('MINISOM', 'whole_text')
+ipOctet = int(config['MINISOM']['ip_octet'])
+somShape = (int(config['MINISOM']['number_of_clusters_x']), int(config['MINISOM']['number_of_clusters_y']))
+numberOfIteration = int(config['MINISOM']['number_of_iteration'])
 sigma = float(config['MINISOM']['sigma'])
-learning_rate = float(config['MINISOM']['learning_rate'])
-random_seed = int(config['MINISOM']['random_seed'])
+learningRate = float(config['MINISOM']['learning_rate'])
+randomSeed = int(config['MINISOM']['random_seed'])
 
 # vytvorenie regexu na odstranovanie specialnych znakov
 delimiter = "!", "\"", "#", "$", "%%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", "\t", "\n"
 regexPattern = '|'.join(map(re.escape, delimiter))
 
-columns = ['id', 'datetime', 'host', 'cef_version', 'cef_vendor', 'cef_dev_prod', 'cef_dev_version', 'cef_event_class_id','cef_name', 'cef_severity', 'src_ip', 'dst_ip', 'src_port', 'dst_port', 'protocol', 'src_mac', 'dst_mac', 'extensions', 'raw', 'src_code', 'dst_code', 'src_country', 'dst_country', 'src_city', 'dst_city', 'src_latitude', 'dst_latitude', 'src_longitude', 'dst_longitude', 'analyzed']
-
-
 def connect_to_db():
+    conn = None
+
     try:
-        conn = psycopg2.connect(host=config['DATABASE']['host'],database=config['DATABASE']['database'], user=config['DATABASE']['user'], password=config['DATABASE']['password'])
-    except:
-        print ("\nI am unable to connect to the database\n")
+        conn = psycopg2.connect(host=config['DATABASE']['host'], database=config['DATABASE']['database'],
+                                user=config['DATABASE']['user'], password=config['DATABASE']['password'])
+    except Exception as e:
+        print(f"\nI am unable to connect to the database -> {e}\n")
     return conn
 
 
@@ -50,136 +46,119 @@ def select_from_db(connection, sql, data):
     try:
         cursor.execute(sql, data)
         data = cursor.fetchall()
-    except:
-        print("\nCannot get data from SecMon databaze!\n")
+    except Exception as e:
+        print(f"\nCannot get data from SecMon databaze -> {e}\n")
     cursor.close()
 
     return data
 
 
-def insert_to_db(connection, som, dataFrameCopy, paddedData):
+def insert_to_db(connection, som, originalData, paddedData):
     cursor = connection.cursor()
 
-    run_sql = (
+    insertRunSql = (
         "INSERT INTO clustered_events_runs (datetime, type_of_algorithm, comment)"
         "VALUES (%s,%s,%s) RETURNING id"
     )
 
-    cluster_sql = (
+    insertClusterSql = (
         "INSERT INTO clustered_events_clusters (comment, fk_run_id)"
         "VALUES (%s,%s) RETURNING id"
     )
 
-    relation_sql = (
+    insertRelationSql = (
         "INSERT INTO clustered_events_relations (fk_run_id, fk_cluster_id, fk_event_id)"
         "VALUES (%s,%s,%s)"
     )
 
-    select_run_statistics_sql = (
-        "SELECT COUNT(*) FROM clustered_events_clusters "
-        "WHERE fk_run_id=%s"
-    )
-
-    update_run_statistics_sql = (
+    updateRunStatisticsSql = (
         "UPDATE clustered_events_runs "
         "SET number_of_clusters=%s "
         "WHERE id=%s"
     )
 
-    select_cluster_statistics_sql = (
+    selectClusterStatisticsSql = (
         "SELECT MAX(cef_severity), COUNT(*) FROM events_normalized "
-        "LEFT JOIN clustered_events_relations " 
+        "LEFT JOIN clustered_events_relations "
         "ON events_normalized.id=clustered_events_relations.fk_event_id "
         "WHERE clustered_events_relations.fk_cluster_id=%s"
     )
 
-    update_clusters_statistics_sql = (
+    updateClustersStatisticsSql = (
         "UPDATE clustered_events_clusters "
         "SET severity=%s, number_of_events=%s "
-        "WHERE id=%s"   
+        "WHERE id=%s"
     )
 
-    data = (datetime.datetime.now(),"miniSOM","")
+    insertRunData = (datetime.datetime.now(), 'miniSOM', '')
 
     try:
         clusters = defaultdict(list)
-        cursor.execute(run_sql, data)
+        cursor.execute(insertRunSql, insertRunData)
         run_id = cursor.fetchone()[0]
 
-        for (event_id, event) in zip(dataFrameCopy["id"], padded_data):
-            winnin_position = som.winner(event)
-            key = winnin_position[1] * som_shape[1] + winnin_position[0]
+        for (event_id, event) in zip(originalData['id'], paddedData):
+            winninPosition = som.winner(event)
+            key = winninPosition[1] * somShape[1] + winninPosition[0]
             clusters[key].append(event_id)
-        
-        for key in clusters:
-            data = ("", run_id)
-            cursor.execute(cluster_sql, data)
-            cluster_id = cursor.fetchone()[0]
-            
-            for item in clusters[key]:
-                data = (run_id, cluster_id, item)
-                cursor.execute(relation_sql, data)
-            
-            data = (cluster_id,)
-            cursor.execute(select_cluster_statistics_sql, data)
-            statistics = cursor.fetchone()
-            data = statistics + (cluster_id,)
-            print(f'{cluster_id} = {data}')
-            cursor.execute(update_clusters_statistics_sql, data)
 
-        data = (run_id,)
-        print(data)
-        cursor.execute(select_run_statistics_sql, data)
-        statistics = cursor.fetchone()[0]
-        data = (statistics, run_id)
-        print(data)
-        cursor.execute(update_run_statistics_sql, data)
-            
+        updateRunStatisticsData = (len(clusters), run_id)
+        cursor.execute(updateRunStatisticsSql, updateRunStatisticsData)
+
+        for key in clusters:
+            insertClusterData = ("", run_id)
+            cursor.execute(insertClusterSql, insertClusterData)
+            clusterId = cursor.fetchone()[0]
+
+            for item in clusters[key]:
+                insertRelationData = (run_id, clusterId, item)
+                cursor.execute(insertRelationSql, insertRelationData)
+
+            selectClusterStatisticsData = (clusterId,)
+            cursor.execute(selectClusterStatisticsSql, selectClusterStatisticsData)
+            statistics = cursor.fetchone()
+            updateClustersStatisticsData = statistics + (clusterId,)
+            cursor.execute(updateClustersStatisticsSql, updateClustersStatisticsData)
+
         connection.commit()
     except Exception as e:
         connection.rollback()
-        print (f"\nI can't execute insert script! : {e}\n")
+        print(f"\nI can't execute insert script! : {e}\n")
     cursor.close()
 
 
 def prepare_data(raw_data):
-    preprocessed_data = ''
+    prepared_data = ''
 
-    for column in columns_to_analyze.split(','):
-        # vyplni sa kazda prazdna bunka hodnotou 0 ktora bude analyzovana
+    # iterate though selected columns
+    for column in columnsToAnalyze.replace(' ', '').split(','):
+        print(f'{column}')
 
-        print(column)
-
+        # fill empty cells with empty string
         raw_data[column] = raw_data[column].fillna('')
 
-        # text nebude obsahovat specialne znaky
-        if clean_text:
-            raw_data[column] = raw_data[column].str.replace(regexPattern, '')
+        # analyze whole ip or specified octet
+        if (column == 'src_ip' or column == 'dst_ip') and config['MINISOM']['ip_octet']:
+            raw_data[column] = raw_data[column].str.replace('.', 'x', ipOctet - 1).str.replace('.', ' ')
+        else:
+            # clean text from special characters
+            if cleanText: raw_data[column] = raw_data[column].str.replace(regexPattern, ' ')
 
-        # analyza textu bude na zaklade celeho textu a nie rozparsovaneho na zaklade medier
-        if whole_text:
-            raw_data[column] = raw_data[column].str.replace(' ', '')
+            # remove white space from text -> analyze according whole text
+            if wholeText: raw_data[column] = raw_data[column].str.replace(' ', '')
 
-        # analyza IP adresy bude podla celku alebo rozparsovana na oktety
-        if (column == 'src_ip' or column == 'dst_ip') and whole_ip:
-            raw_data[column] = raw_data[column].str.replace('.', '')
-
-        # analyza MAC adresy bude podla celku alebo rozparsovana na oktety
-        if (column == 'src_mac' or column == 'dst_mac') and whole_mac:
-            raw_data[column] = raw_data[column].str.replace(':', '')
-
-        # polia okrem prveho sa napajaju na seba medzerov
-        if isinstance(preprocessed_data, np.ndarray):
-            preprocessed_data = np.char.add(preprocessed_data, ' ')
+        # add white space bettwen rows
+        if isinstance(prepared_data, np.ndarray):
+            prepared_data = np.char.add(prepared_data, ' ')
 
         # spojenie stlpcov do jedneho
-        preprocessed_data = np.char.add(preprocessed_data, raw_data[column].tolist())
+        prepared_data = np.char.add(prepared_data, raw_data[column].tolist())
 
-    return preprocessed_data.tolist()
+    return prepared_data.tolist()
 
 
-def encode_data(choosen_data):
-    return [one_hot(item, 1000000) for item in choosen_data]
+def encode_data(prepared_data):
+    return [one_hot(item, 1000000) for item in prepared_data]
 
 
 def normalize_data(encode_data):
@@ -191,7 +170,6 @@ def normalize_data(encode_data):
     for array in encode_data:
         normalized = [float(item - min_value) / (max_value - min_value) for item in array]
         normalized_data.append(normalized)
-        normalized = []
 
     return normalized_data
 
@@ -204,52 +182,86 @@ def padding_data(normalized_data, size_of_longest_data):
     return pad_sequences(normalized_data, maxlen=size_of_longest_data, padding='post', dtype=float)
 
 
-def miniSOM(padded_data, size_of_longest_data):
-    som = MiniSom(som_shape[0], som_shape[1], size_of_longest_data,
+def miniSOM(paddedData, sizeOfLongestData):
+    som = MiniSom(somShape[0], somShape[1], sizeOfLongestData,
                   sigma=sigma,
-                  learning_rate=learning_rate,
-                  random_seed=random_seed)
-    som.train(np.array(padded_data), number_of_iteration, random_order=True, verbose=True)
+                  learning_rate=learningRate,
+                  random_seed=randomSeed)
+    som.train(np.array(paddedData), numberOfIteration, random_order=True, verbose=True)
     return som
 
 
 if __name__ == '__main__':
-          
-    #connect to SecMon database
+
+    # connect to SecMon database
     connection = connect_to_db()
 
-    #get rawData from events_normalized table
-    if config['MINISOM']['not_older_than']:
-        sql = "SELECT * FROM events_normalized WHERE datetime >= %s ORDER BY datetime DESC LIMIT %s"
-        data = (config['MINISOM']['not_older_than'], config['MINISOM']['number_of_events'])
-    else:
-        sql = "SELECT * FROM events_normalized ORDER BY datetime DESC LIMIT %s"
-        data = (config['MINISOM']['number_of_events'],)
+    # get rawData from events_normalized table
+    selectSql = "SELECT * FROM events_normalized"
+    selectData = ()
 
-    rawData = select_from_db(connection, sql, data)
+    if config['MINISOM']['not_older_than']:
+        selectSql += " WHERE datetime >= %s"
+        selectData = (config['MINISOM']['not_older_than'],)
+
+    selectSql += " ORDER BY datetime DESC"
+
+    if config['MINISOM']['number_of_events']:
+        selectSql += " LIMIT %s"
+        selectData = selectData + (config['MINISOM']['number_of_events'],)
+
+    rawData = select_from_db(connection, selectSql, selectData)
 
     if not rawData:
         exit("Input data are empty!")
 
-    #get column headers from events_normalized table
-    columnHeaders = [columnName[0] for columnName in select_from_db(connection, "SELECT column_name FROM information_schema.columns WHERE table_schema = 'public' AND table_name = %s", ('events_normalized',))]
+    # get column headers from events_normalized table
+    headersSql = (
+        "SELECT column_name FROM information_schema.columns "
+        "WHERE table_schema = 'public' AND table_name = %s"
+    )
+    headersData = ('events_normalized',)
 
-    #convert rawData from events_normalized table to dataFrame
+    columnHeaders = [columnName[0] for columnName in select_from_db(connection, headersSql, headersData)]
+
+    # convert rawData from events_normalized table to dataFrame
     dataFrame = pandas.DataFrame(rawData, columns=columnHeaders, dtype=str)
-   
-    #print(dataFrame)
-    
-    #create copy of dataFrame
     dataFrameCopy = dataFrame.copy()
 
-    
-    prepared_data = prepare_data(dataFrame)
-    encoded_data = encode_data(prepared_data)
-    normalized_data = normalize_data(encoded_data)
-    size_of_longest_data = get_size_of_longest_data(normalized_data)
-    padded_data = padding_data(normalized_data, size_of_longest_data)
-    som = miniSOM(padded_data, size_of_longest_data)
-    insert_to_db(connection, som, dataFrameCopy, padded_data)    
-    
+    # select columns and create list of rows
+    print('Preparing data...')
+    preparedData = prepare_data(dataFrame)
+    print(f'{np.array(preparedData)}\n')
+
+    # encode list of strings
+    print('Encoding data...')
+    encodedData = encode_data(preparedData)
+    print(f'{np.array(encodedData)}\n')
+
+    # normalize data to <0,1>
+    print('Normalizing data...')
+    normalizedData = normalize_data(encodedData)
+    print(f'{np.array(normalizedData)}\n')
+
+    # get longes list
+    print('Finding longest data...')
+    sizeOfLongestData = get_size_of_longest_data(normalizedData)
+    print(f'Size of longes data -> {sizeOfLongestData}\n')
+
+    # add padding according longest list
+    print('Padding unevenly data...')
+    paddedData = padding_data(normalizedData, sizeOfLongestData)
+    print(f'{np.array(paddedData)}\n')
+
+    # start miniSOM
+    print('Starting miniSOM...')
+    som = miniSOM(paddedData, sizeOfLongestData)
+    print('Ending miniSOM...\n')
+
+    # insert to database
+    print('Inserting to db...')
+    insert_to_db(connection, som, dataFrameCopy, paddedData)
+    print('Finishing run...')
+
     connection.close()
-    print("End")
+
