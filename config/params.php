716923f01c833c5a6ad3411b7dc3e1f825b0b8f9
Miroslav Pavlak <mpavlak3@gmail.com>
2022-07-02 10:03:18 +0200

    Update .gitignore

--- config/params.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'defaultPass' => 'password'
-];
--- config/urls.php
+++
@@ -1,5 +0,0 @@
-<?php
-return [
-	['class' => 'yii\rest\UrlRule', 'controller' => ['event' => 'api/event'], 'prefix' => 'api', 'pluralize' => false],
-    ['class' => 'yii\rest\UrlRule', 'controller' => ['event-type' => 'api/event-type'], 'prefix' => 'api', 'pluralize' => false],
-];
