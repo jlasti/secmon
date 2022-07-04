ade17c7deb79bbb9c7d58c0c609950b3bc4d7fbb
Miroslav Pavlak <mpavlak3@gmail.com>
2022-07-04 10:22:25 +0200

    Update deployment scripts

--- secmon_deploy.sh
+++ secmon_deploy.sh
@@ -5,23 +5,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NORMAL='\033[0m'
 
-echo -e "Installing usefull packages"
-#sudo yum install -y firewalld rsyslog
-
-#sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
-#sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
-#sudo pip3.6 install -U configparser
-#sudo pip3.6 install termcolor
-
-echo -e "Setting up firewall"
-#sudo firewall-cmd --permanent --add-port=80/tcp
-#sudo firewall-cmd --permanent --add-port=443/tcp
-#sudo firewall-cmd --permanent --add-port=514/tcp
-#sudo firewall-cmd --reload
-
-#sudo mkdir /var/log/secmon
-#sudo chmod 777 /var/log/secmon
-
 echo -e "Copying config files"
 #sudo cp deployment/config_files/rsyslog.conf /etc/
 #sudo cp deployment/config_files/logrotate.conf /etc/logrotate.d/secmon
--- secmon_preconfig.sh
+++ secmon_preconfig.sh
@@ -4,7 +4,6 @@ sudo yum -y update
 
 echo -e "Installing usefull packages"
 sudo yum install -y firewalld rsyslog
-
 sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
 sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
 sudo pip3.6 install -U configparser
@@ -16,11 +15,13 @@ sudo firewall-cmd --permanent --add-port=443/tcp
 sudo firewall-cmd --permanent --add-port=514/tcp
 sudo firewall-cmd --reload
 
+echo -e "Creating log directory"
 sudo mkdir /var/log/secmon
 sudo chmod 777 /var/log/secmon
 
-echo -e "Copying config files"
+echo -e "Copying rsyslog and logrotate config files"
 sudo cp deployment/config_files/rsyslog.conf /etc/
 sudo cp deployment/config_files/logrotate.conf /etc/logrotate.d/secmon
 
-sudo python3 secmon_manaher.py deploy
+echo -e "Secmon preconfiguration is complete, to deploy SecMon run command \"python3 secmon_manaher.py deploy\""
+python3 secmon_manaher.py deploy
