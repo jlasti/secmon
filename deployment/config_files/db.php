1a6c5c3f1c9d30bf68740faf4ee4ad72d674542a
Miroslav Pavlak <mpavlak3@gmail.com>
2022-07-26 16:09:40 +0200

    Renaming db host

--- deployment/config_files/db.php
+++ deployment/config_files/db.php
@@ -1,7 +1,7 @@
 <?php
 return [
 	'class' => 'yii\db\Connection',
-        'dsn' => 'pgsql:host=db;port=5432;dbname=secmon',
+        'dsn' => 'pgsql:host=secmon_db;port=5432;dbname=secmon',
         'username' => 'secmon',
         'password' => '<password>',
         'charset' => 'utf8',
