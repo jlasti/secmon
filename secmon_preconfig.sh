9b6901a1056927438699bc1aad43ec72e463d5a4
Miroslav Pavlak <43376614+Miropanak@users.noreply.github.com>
2022-08-22 14:46:00 +0200

    Update secmon_preconfig.sh

--- secmon_preconfig.sh
+++ secmon_preconfig.sh
@@ -5,23 +5,6 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NORMAL='\033[0m'
 
-sudo yum clean all
-sudo yum -y update
-
-echo -e "Installing usefull packages"
-sudo yum install -y firewalld rsyslog
-#Installation of python packages on CentOS 7
-#sudo yum install -y https://repo.ius.io/ius-release-el7.rpm
-#sudo yum install -y python36u python36u-libs python36u-devel python36u-pip
-sudo pip3.6 install -U configparser
-
-echo -e "Setting up firewall"
-sudo firewall-cmd --permanent --add-port=80/tcp
-sudo firewall-cmd --permanent --add-port=443/tcp
-sudo firewall-cmd --permanent --add-port=514/tcp
-sudo firewall-cmd --reload
-echo -e "${GREEN}Done${NORMAL}"
-
 echo -e "Creating log directory"
 sudo mkdir /var/log/secmon
 sudo chmod 777 /var/log/secmon
