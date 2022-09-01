bec247bb20be36b52ad812956ed6ff6ada92d662
Miroslav Pavlak <mpavlak3@gmail.com>
2022-09-01 12:56:47 +0200

    Add extractCefFields function

--- models/SecurityEvents.php
+++ models/SecurityEvents.php
@@ -3,7 +3,6 @@
 namespace app\models;
 
 use Yii;
-use app\models\Event;
 use GeoIp2\Database\Reader;
 use GeoIp2\Exception\AddressNotFoundException;
 use MaxMind\Db\Reader\InvalidDatabaseException;
@@ -439,4 +438,220 @@ class SecurityEvents extends \yii\db\ActiveRecord
     {
         return $this->hasMany(ClusteredEventsRelations::className(), ['fk_event_id' => 'id']);
     }
+    
+    public static function extractCefHeader($cefString)
+	{
+		$event = new self();
+
+		$data = explode('|', $cefString);
+
+		$dateHost = explode(' ', strrev(array_shift($data)), 3);
+		$event->cef_version = str_replace('CEF:', '', strrev(array_shift($dateHost)));
+        $event->device_host_name= strrev(array_shift($dateHost));
+        $event->type = 2;
+        
+        // osetrenie ak na zaciatku logu nie je timestamp
+        $strDate = array_shift($dateHost);
+        if(!empty($strDate))
+        {
+		    $event->datetime = date('Y-m-d H:i:s', strtotime(strrev($strDate)));
+        }
+        else
+        {
+            date_default_timezone_set('Europe/Bratislava');
+            $event->datetime = date('Y-m-d H:i:s');
+        }
+
+		$event->cef_vendor = array_shift($data);
+		$event->cef_device_product = array_shift($data);
+		$event->cef_device_version = array_shift($data);
+		$event->cef_event_class_id = array_shift($data);
+		$event->cef_name = array_shift($data);
+		$event->cef_severity = array_shift($data);
+
+        $data = array_shift($data);
+        $exData = explode(" ", $data);
+        $values = [];
+        foreach($exData as $val) {
+            $tmp = explode("=", $val);
+            if($tmp[0] == "rawEvent" || $tmp[0] == "reason")
+            {
+                break;
+            }
+            $values[$tmp[0]] = isset($tmp[1]) ? $tmp[1] : "";
+        }
+
+        $event->parent_events = $values['cs1'] ?? "";
+        $event->attack_type = $values['att'] ?? "";
+		$event->raw_event = $cefString;
+
+		return $event;
+	}
+
+    public static function extractCefFields($cefString)
+	{
+		$event = new self();
+        $event = $event->extractCefHeader($cefString);
+        $event->type = 0;
+
+		$data = explode('|', $cefString);
+
+		for($i = 0; $i < 7; $i++)
+		{
+			array_shift($data);
+		}
+
+		$data = array_shift($data);
+	
+		preg_match('/rawEvent=(.*)/', $data, $matches);
+
+		$raw = "";
+		if(empty($matches))
+		{
+			$raw = null;
+		}
+		else
+		{
+			$raw = $matches[1];
+		}
+
+		$exData = explode(" ", $data);
+		$values = [];
+		foreach($exData as $val) {
+			$tmp = explode("=", $val);
+			if($tmp[0] == "rawEvent" || $tmp[0] == "reason")
+			{
+				break;
+			}
+            		$values[$tmp[0]] = isset($tmp[1]) ? $tmp[1] : "";
+		}
+		
+		//nevyhnutne dva if-y, lebo delimiter " " a "=" kazia vstup do atributov request_url a request_client_application
+		if(preg_match('/request=/', $data)){
+			$start_pos = strpos($data, 'request=');
+			$start_pos += strlen('request=');
+			$end_pos = strpos($data, ' ', $start_pos);
+			$length = $end_pos - $start_pos;
+			$request_url = substr($data, $start_pos, $length);
+		}
+		if(preg_match('/requestClientApplication=/', $data)){
+			$start_pos = strpos($data, 'requestClientApplication=');
+			$start_pos += strlen('requestClientApplication=');
+			$end_pos = strpos($data, ' rawEvent=', $start_pos);
+			$length = $end_pos - $start_pos;
+			$request_client_application = substr($data, $start_pos, $length);
+		}	
+
+		$event->source_address = $values['src'] ?? "";
+		if($event->source_address == "localhost"){
+			$event->source_address = "127.0.0.1";
+		}
+		$event->destination_address = $values['dst'] ?? "";
+		$event->source_mac_address = $values['smac'] ?? "";
+		$event->destination_mac_address = $values['dmac'] ?? "";
+		$event->source_port = $values['spt'] ?? "";
+		$event->destination_port = $values['dpt'] ?? "";
+		$event->application_protocol = $values['proto'] ?? $values['app'] ?? "";
+		$event->request_method = $values['request_method'] ?? "";
+		$event->request_url = $request_url ?? "";
+		$event->request_client_application = $request_client_application ?? "";
+		$event->destination_user_name = $values['duser'] ?? "";
+		$event->destination_user_id = $values['duid'] ?? "";
+		$event->destination_group_name = $values['cs1'] ?? "";
+		$event->destination_group_id = $values['cn1'] ?? "";
+		$event->device_process_id = $values['dvcpid'] ?? "";
+		$event->source_user_privileges = $values['spriv'] ?? "";
+		$event->destination_user_name = $values['cs2'] ?? "";
+		$event->raw_event = $raw;
+
+		//map netwrok model for src IP
+		$position = strpos($cefString, "src_network_model_id=");
+		if($position != FALSE){
+            $event->source_ip_network_model = $cefString[$position + strlen("src_network_model_id=")];
+		}
+
+		//map network model for dst IP
+		$position = strpos($cefString, "dst_network_model_id=");
+		if($position != FALSE){
+            $event->destination_ip_network_model = $cefString[$position + strlen("dst_network_model_id=")];
+		}
+		
+		//map geoIP for src IP
+		$position = strpos($cefString, "src_country_isoCode=");
+		if($position != FALSE){
+            $start_position = $position + strlen("src_country_isoCode=");
+            $end_position = strpos($cefString, " ", $start_position);
+            $event->source_code = substr($cefString, $start_position, $end_position - $start_position);
+		}
+		
+
+		$position = strpos($cefString, "src_country_name=");
+		if($position != FALSE){
+            $start_position = $position + strlen("src_country_name=");
+            $end_position = strpos($cefString, "src_", $start_position) - 1;
+            $event->source_country = substr($cefString, $start_position, $end_position - $start_position);
+		}
+
+		$position = strpos($cefString, "src_city_name=");
+		if($position != FALSE){
+            $start_position = $position + strlen("src_city_name=");
+            $end_position = strpos($cefString, "src_", $start_position) - 1;
+            $event->source_city = substr($cefString, $start_position, $end_position - $start_position);
+		}
+
+		$position = strpos($cefString, "src_location_latitude=");
+		if($position != FALSE){
+            $start_position = $position + strlen("src_location_latitude=");
+            $end_position = strpos($cefString, " ", $start_position);
+            $event->source_latitude = substr($cefString, $start_position, $end_position - $start_position);
+		}
+
+		$position = strpos($cefString, "src_location_longitude=");
+		if($position != FALSE){
+            $start_position = $position + strlen("src_location_longitude=");
+            $end_position = strpos($cefString, " ", $start_position);
+            $event->source_longitude = substr($cefString, $start_position, $end_position - $start_position);
+		}
+		
+
+		//map geoIP for dst IP
+		$position = strpos($cefString, "dst_country_isoCode=");
+		if($position != FALSE){
+            $start_position = $position + strlen("dst_country_isoCode=");
+            $end_position = strpos($cefString, " ", $start_position);
+            $event->destination_code = substr($cefString, $start_position, $end_position - $start_position);
+		}
+
+		$position = strpos($cefString, "dst_country_name=");
+		if($position != FALSE){
+            $start_position = $position + strlen("dst_country_name=");
+            $end_position = strpos($cefString, "dst_", $start_position) - 1;
+            $event->destination_country = substr($cefString, $start_position, $end_position - $start_position);
+		}
+
+		$position = strpos($cefString, "dst_city_name=");
+		if($position != FALSE){
+            $start_position = $position + strlen("dst_city_name=");
+            $end_position = strpos($cefString, "dst_", $start_position) - 1;
+            $event->destination_city = substr($cefString, $start_position, $end_position - $start_position);
+		}
+
+		$position = strpos($cefString, "dst_location_latitude=");
+		if($position != FALSE){
+            $start_position = $position + strlen("dst_location_latitude=");
+            $end_position = strpos($cefString, " ", $start_position);
+            $event->destination_latitude = substr($cefString, $start_position, $end_position - $start_position);
+		}
+
+		$position = strpos($cefString, "dst_location_longitude=");
+		if($position != FALSE){
+            $start_position = $position + strlen("dst_location_longitude=");
+            $end_position = strpos($cefString, " ", $start_position);
+            $event->destination_longitude = substr($cefString, $start_position, $end_position - $start_position);
+		}
+
+		$event->cef_severity = SeverityCalculator::calculateSeverity($event);
+
+		return $event;
+	}
 }
