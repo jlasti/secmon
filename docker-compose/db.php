cbc83f18df8a8e35d51ac66c973a1ab10c27c814
Miroslav Pavlak <43376614+Miropanak@users.noreply.github.com>
2023-10-28 22:53:49 +0200

    Delete docker-compose directory

--- docker-compose/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-	'class' => 'yii\db\Connection',
-        'dsn' => 'pgsql:host=db;port=5432;dbname=secmon',
-        'username' => 'secmon',
-        'password' => '<password>',
-        'charset' => 'utf8',
-];
-?>
